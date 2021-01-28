ee76636880eeb7f909b8701786cfb97bbfca3f40
Austin-Garrison <78004903+Austin-Garrison@users.noreply.github.com>
2021-01-28 13:51:22 -0800

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,7 +1,10 @@
 /* 
 * Author: Austin Garrison
 * Lab: 1
-* Problem statement:  Write a program that prints the numbers from 1 to 100, but for multiples of 3 print “Fizz” instead of the number and for multiples of 5 print “Buzz”. For numbers that are multiples of both three and five print “FizzBuzz”.
+* Problem statement:  Write a program that prints the numbers from 1 to 100, 
+* but for multiples of 3 print “Fizz” instead of the number and for multiples 
+* of 5 print “Buzz”. For numbers that are multiples of both three and five 
+* print “FizzBuzz”.
 *
 */
 
