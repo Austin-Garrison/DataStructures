88c6b49890241ad0eb381e661c42d7fad3d660bd
Austin-Garrison <78004903+Austin-Garrison@users.noreply.github.com>
2021-01-28 13:54:16 -0800

    Update FizzBuzz.java

--- Lab1/FizzBuzz.java
+++ Lab1/FizzBuzz.java
@@ -1,1 +1,33 @@
+/* 
+* Author: Austin Garrison
+* Lab: 1
+* Problem statement:  Write a program that prints the numbers from 1 to 100, 
+* but for multiples of 3 print “Fizz” instead of the number and for multiples 
+* of 5 print “Buzz”. For numbers that are multiples of both three and five 
+* print “FizzBuzz”.
+*
+*/
 
+class Main {
+  public static void main(String[] args) {
+
+    for (int i = 1; i <= 100; i++) {
+      if (i % 15 == 0) {
+        System.out.println("FizzBuzz");
+      }
+
+      else if (i % 3 == 0) {
+        System.out.println("Fizz");
+      }
+
+      else if (i % 5 == 0) {
+        System.out.println("Buzz");
+      }
+
+      else {
+        System.out.println(i);
+      }
+    }
+    
+  }
+}
